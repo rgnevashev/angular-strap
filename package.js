0841c4b6903d37ee30e9686165d54de58b493005
Roman <rgnevashev@gmail.com>
2015-12-20 21:40:14 +0300

    Update package.js

--- package.js
+++ package.js
@@ -1,24 +1,15 @@
-// Package metadata file for Meteor.js
-var packageName = 'mgcrea:angular-strap';
-var where = 'client';
-var version = '2.3.6';
-var summary = 'AngularJS 1.2+ native directives AND templates for Bootstrap 3.';
-var gitLink = 'https://github.com/mgcrea/angular-strap';
-var documentationFile = 'README.md';
-
-// Meta-data
 Package.describe({
-  name: packageName,
-  version: version,
-  summary: summary,
-  git: gitLink,
-  documentation: documentationFile
+  name: 'mgcrea:angular-strap',
+  version: '2.3.6',
+  summary: 'AngularJS 1.2+ native directives AND templates for Bootstrap 3.',
+  git: 'https://github.com/mgcrea/angular-strap',
+  documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.addFiles([
-  'dist/angular-strap.js',
-  'dist/angular-strap.tpl.js'
-  ], where);
+    "dist/angular-strap.js",
+    "dist/angular-strap.tpl.js"
+  ], 'client');
 }); 
