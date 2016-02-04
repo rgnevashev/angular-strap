8bd78fae34a0daa1eab3fce9f41ab48c41f949e7
roman <rgnevashev@gmail.com>
2016-02-04 08:55:03 +0300

    update

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
   name: 'mgcrea:angular-strap',
-  version: '2.3.6',
+  version: '2.3.7',
   summary: 'AngularJS 1.2+ native directives AND templates for Bootstrap 3.',
   git: 'https://github.com/mgcrea/angular-strap',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
+  //api.versionsFrom('1.2.1');
   api.addFiles([
     "dist/angular-strap.js",
     "dist/angular-strap.tpl.js"
   ], 'client');
-}); 
+});
